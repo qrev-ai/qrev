ecf805c04ca5cbb54fdf007df8092389c577d10b
leeparnell <152523161+leeparnell@users.noreply.github.com>
2024-05-02 16:38:42 -0500

    Updated unit tests

--- ai/projects/server/tests/test_serve_remote.py
+++ ai/projects/server/tests/test_serve_remote.py
@@ -1,26 +1,16 @@
 import os
 import unittest
-from dataclasses import dataclass
-from typing import Dict, List
+from distutils.util import strtobool
 
 import chromadb
 from chromadb.config import Settings
-from flask import Flask
-
-from qai.ai.utils.token_utils import num_tokens
 from qai.chat.config import cfg
-from qai.chat.layers.llm.chatbot import LLM, Chatbot
-from qai.chat.layers.query import Query, QueryReturn
-from qai.chat.prompt.config import PromptConfig
-from distutils.util import strtobool
-
-
-if strtobool(os.getenv("TEST_LOCAL_ONLY", "False")):
-    print("Skipping remote test")
-    exit()
-
 
 from qai.server.serve import app
+
+REMOTE_TEST = strtobool(os.getenv("TEST_REMOTE", "False"))
+if not REMOTE_TEST:
+    print("Skipping remote tests for test_server_remote.py. enable by TEST_REMOTE=True")
 
 if "CHROMA_SERVER_AUTH_CREDENTIALS" in os.environ:
     settings = Settings(
@@ -38,6 +28,8 @@ class TestServing(unittest.TestCase):
         pass
 
     def test_query_with_chroma_query(self):
+        if not REMOTE_TEST:
+            return
         client = app.test_client()
 
         with app.test_request_context():
@@ -55,19 +47,13 @@ class TestServing(unittest.TestCase):
                     "mock": False,
                     "user_id": "1",
                     "company_name": "Ardvaarks R Us",
-                    # "company_id": "50d4a259-388c-4ec8-aca5-c4ea4e69d3c1",
-                    # "verbose_return": True,
-                    # "use_context": True,
-                    # "use_history": False,
                 },
             )
             print(f"result of test_query_with_chroma_query: {r.get_json()}")
-            # self.assertEqual(r.status_code, 200)
-
-            # self.assertEqual(r.get_json()["response"], "Mocked Response")
-
 
     def test_campaign(self):
+        if not REMOTE_TEST:
+            return
         client = app.test_client()
         with app.test_request_context():
             r = client.get(
@@ -77,14 +63,15 @@ class TestServing(unittest.TestCase):
                     "query": "what is the deductible for a family plan?",
                     "mock": True,
                     "user_id": "1",
-                    "company_id": "aaca9cd7-959a-4a31-93a8-b524cb58ff05",
+                    "company_id": "testid1",
                 },
             )
             print(f"result of test_campaign: {r.get_json()}")
             self.assertEqual(r.status_code, 200)
 
+
 if __name__ == "__main__":
-    test_name = "test_campaign"
+    test_name = ""
     if test_name:
         suite = unittest.TestSuite()
         suite.addTest(TestServing(test_name))
