ce251bc382ad673bcbeb5a5911e70d7a875e2d7f
Tarun Chawla <tc.chawla2000@gmail.com>
2024-05-09 18:49:02 -0400

    Merge branch 'main' into mui-grid

--- client/src/pages/campaigns/EmailsTable.tsx
+++ client/src/pages/campaigns/EmailsTable.tsx
@@ -96,3 +96,102 @@ const EmailsTable = ({ data }: EmailsTableProps): React.ReactElement => {
 };
 
 export default EmailsTable;
+
+'use strict';
+
+import { useEffect, useMemo, useRef, useState } from 'react';
+import moment from 'moment';
+import { CampaignEmailsType } from '../../models/campaigns';
+
+import { AgGridReact } from '@ag-grid-community/react';
+import '@ag-grid-community/styles/ag-grid.css';
+import '@ag-grid-community/styles/ag-theme-alpine.css';
+import { ColDef } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+ModuleRegistry.registerModules([ClientSideRowModelModule, ExcelExportModule]);
+
+const ChipComponent = ({ value }: { value: string }) => (
+  <span className="rounded-full px-2 py-1 bg-[#e8e0e8] text-xs">{value}</span>
+);
+const RenderComponent = ({ value }: { value: string }) => <span>{value}</span>;
+
+interface EmailsTableProps {
+  data?: CampaignEmailsType;
+}
+
+const EmailsTable = ({ data }: EmailsTableProps): React.ReactElement => {
+  const gridRef = useRef<AgGridReact>(null);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+  const [rowData, setRowData] = useState<any[]>();
+  const [columnDefs, setColumnDefs] = useState<ColDef[]>();
+  const defaultColDef = useMemo<ColDef>(() => {
+    return {
+      flex: 1,
+      editable: true,
+      cellDataType: false,
+      sortable: true,
+      filter: true,
+      resizable: true,
+    };
+  }, []);
+
+  useEffect(() => {
+    if (data?.headers) {
+      const keys = Object.keys(data.headers);
+      const values = Object.values(data.headers);
+      const columnDefs = values
+        ?.sort((a, b) => (a.order > b.order ? 1 : -1))
+        ?.map((item, index) => ({
+          field: keys[index],
+          headerName: item.label,
+          hide: item.hidden,
+          cellRenderer: (params: { data: any }) =>
+            item.type === 'chip' ? (
+              <ChipComponent value={params?.data?.[keys[index]]} />
+            ) : (
+              <RenderComponent value={params?.data?.[keys[index]]} />
+            ),
+        }));
+      setColumnDefs(columnDefs);
+    }
+
+    if (data?.data) {
+      const rowData = data.data?.map((item) => ({
+        ...item,
+        created_on: moment(new Date(item.created_on)).format('DD/MM/YYYY, h:mm a'),
+        scheduled_time: moment(new Date(item.scheduled_time)).format('DD/MM/YYYY, h:mm a'),
+      }));
+      setRowData(rowData);
+    }
+  }, [data]);
+
+  const exportCSV = () => {
+    gridRef?.current?.api?.exportDataAsExcel({ fileName: 'Campaign Sequences Emails' });
+  };
+
+  return (
+    <div className="flex flex-col h-[calc(100%-160px)] bg-white">
+      <div className="flex items-center justify-end mb-4">
+        <button className="border border-[#2F4858] py-1 px-2 rounded text-sm" onClick={exportCSV}>
+          Export CSV
+        </button>
+      </div>
+      <div style={gridStyle} className={'ag-theme-alpine'}>
+        <AgGridReact
+          ref={gridRef}
+          animateRows
+          deltaSort
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          editType={'fullRow'}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EmailsTable;
