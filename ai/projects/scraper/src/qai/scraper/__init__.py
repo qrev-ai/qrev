82351725623f58a8746defbb9e1dce15706d6c89
leeparnell <152523161+leeparnell@users.noreply.github.com>
2024-08-05 09:49:50 -0700

    Version updates

--- ai/projects/scraper/src/qai/scraper/__init__.py
+++ ai/projects/scraper/src/qai/scraper/__init__.py
@@ -1,8 +1,6 @@
 import os
 
-# import pkg_resources
 from pi_conf import load_config
-
 from qai.scraper.filters.filter import Filter as Filter
 from qai.scraper.scrapers.scraper import Scraper as Scraper
 
@@ -14,7 +12,7 @@ DATA_DIR = os.path.join(ROOT_DIR, "..", "data")
 PARSE_CONFIG_DIR = os.path.join(ROOT_DIR, "..", "parse_configs")
 try:
     import importlib.metadata
-    # VERSION = pkg_resources.get_distribution("qai.scraper").version
+
     VERSION = importlib.metadata.version("qai.scraper")
 except Exception:
     VERSION = "0.0.0"
