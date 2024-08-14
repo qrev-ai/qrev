915cb5673109d713b4a0a39142c55dd9a9914a4d
sanjay-gr-qrev <sanjay@qrev.ai>
2024-08-14 10:41:53 +0530

    Set a cron job (every 1hr) for auto-refreshing Google Pub Sub subscription before it expires since Google PubSub subscription expires every 7 days if not refreshed.

--- server/cron.setup.js
+++ server/cron.setup.js
@@ -2,6 +2,7 @@ import cron from "node-cron";
 import { v4 as uuidv4 } from "uuid";
 
 import * as CampaignUtils from "./utils/campaign/campaign.utils.js";
+import * as GoogleUtils from "./utils/google/google.auth.utils.js";
 
 import { logger } from "./logger.js";
 
@@ -11,6 +12,15 @@ export function cronSetup() {
         const txid = uuidv4();
         await CampaignUtils.executeCampaignCronJob(
             {},
+            { txid, sendErrorMsg: true }
+        );
+    });
+
+    //set a cron job for every 1 hour
+    cron.schedule("0 0 */1 * * *", async () => {
+        const txid = uuidv4();
+        await GoogleUtils.autoRefreshGooglePubSubWebhook(
+            { expiresInNextNMinutes: 62 },
             { txid, sendErrorMsg: true }
         );
     });
