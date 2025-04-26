434ac1ebffe63fce0144f266dae8abfc09cf1f58
sanjay-gr-qrev <sanjay@qrev.ai>
2025-04-26 13:29:09 +0530

    refactor(crm): Remove CRM API and routes
    Delete the CRM API and associated routes from the application, streamlining the codebase by removing unused functionality. This change enhances maintainability and reduces complexity in the routing structure.

--- server/routes.js
+++ server/routes.js
@@ -8,7 +8,6 @@ import HubSpotRouter from "./routes/integration/hubspot.routes.js";
 import ZoomRouter from "./routes/integration/zoom.routes.js";
 import QaiBotRouter from "./routes/qai/qai.routes.js";
 import CampaignRouter from "./routes/campaign/campaign.routes.js";
-import CrmRouter from "./routes/crm/crm.routes.js";
 import AgentRouter from "./routes/agents/agent.routes.js";
 
 export function setRoutes(app) {
@@ -25,6 +24,5 @@ export function setRoutes(app) {
     app.use("/api/zoom", ZoomRouter);
     app.use("/api/qai", QaiBotRouter);
     app.use("/api/campaign", CampaignRouter);
-    app.use("/api/crm", CrmRouter);
     app.use("/api/agent", AgentRouter);
 }
